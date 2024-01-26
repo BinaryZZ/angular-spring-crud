deae675a16b745ea6ec9491182a742d6fb82f458
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-26 00:58:05 -0300

    Created a service to inject dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Course } from '../model/course';
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -12,10 +13,16 @@ import { AppMaterialModule } from '../../shared/app-material/app-material.module
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
+
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService ){
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+
+  }
 
   ngOnInit(): void {
     //throw new Error('Method not implemented.');
